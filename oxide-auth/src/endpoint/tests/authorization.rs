3471e349ef91424f1987224cf8daff35b62121e7
Abe Kohandel <abe@kodebooth.com>
2026-01-19 15:37:05 -0800

    clippy(oxide-auth): resolve search_is_some
    Resolve the search_is_some warning.
    
    The warning addressed by this change can be seen below:
    
    warning: called `is_none()` after calling `find()` on a string
       --> oxide-auth/src/endpoint/tests/pkce.rs:128:17
        |
    128 |         assert!(response.location.unwrap().as_str().find("error").is_none());
        |                 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ help: consider using: `!response.location.unwrap().as_str().contains("error")`
        |
        = help: for further information visit https://rust-lang.github.io/rust-clippy/rust-1.92.0/index.html#search_is_some
    
    Link: https://rust-lang.github.io/rust-clippy/rust-1.92.0/index.html#search_is_some
    Signed-off-by: Abe Kohandel <abe@kodebooth.com>

--- oxide-auth/src/endpoint/tests/authorization.rs
+++ oxide-auth/src/endpoint/tests/authorization.rs
@@ -46,7 +46,7 @@ impl AuthorizationSetup {
         assert_eq!(response.status, Status::Redirect);
 
         match response.location {
-            Some(ref url) if url.as_str().find("error").is_none() => (),
+            Some(ref url) if !url.as_str().contains("error") => (),
             other => panic!("Expected successful redirect: {:?}", other),
         }
     }
